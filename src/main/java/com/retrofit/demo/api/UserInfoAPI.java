d6cc620c3d60de283d333ee58bba5b625b98882a
goofwoof <heli0086123456>
2021-01-14 17:59:26 +0800

    new retry interface

--- src/main/java/com/retrofit/demo/api/UserInfoAPI.java
+++ src/main/java/com/retrofit/demo/api/UserInfoAPI.java
@@ -5,6 +5,8 @@ import com.retrofit.demo.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 /**
  * @author puthlive
  */
@@ -12,9 +14,20 @@ import org.springframework.web.bind.annotation.*;
 public class UserInfoAPI {
     @Autowired
     private UserInfoService userInfoService;
+    private static int time_error = 1;
 
     @RequestMapping(value = "/getUserInfo", method = {RequestMethod.POST, RequestMethod.GET})
-    public Object getUserInfo(@RequestParam String id) {
+    public Object getUserInfo(@RequestParam String id) throws Exception {
+        return userInfoService.getUserInfo(id);
+    }
+
+    @RequestMapping(value = "/getUserInfoRetry", method = {RequestMethod.POST, RequestMethod.GET})
+    public Object getUserInfoRetry(@RequestParam String id) throws Exception {
+        if (time_error % 10 != 0){
+            time_error++;
+            throw new IOException("this time"+ time_error + "is retry error");
+        }
+        time_error=1;
         return userInfoService.getUserInfo(id);
     }
 
